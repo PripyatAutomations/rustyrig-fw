7e800f39862d1d06e2734c6d44522c39032f3c86
rustyaxe <netrig@istabpeople.com>
2025-05-06 11:35:12 -0400

    minor cleanup

--- count-lines.sh
+++ count-lines.sh
@@ -5,7 +5,6 @@ SZ_JS=$(cat www/js/webui*.js |wc -l)
 SZ_HTML=$(cat www/index.html | wc -l)
 SZ_CSS=$(cat www/css/*.css | wc -l)
 SZ_SH=$(cat *.sh | wc -l)
-SZ_JSON=$(find . -name \*.json|xargs cat |  wc -l)
-SZ_TTL=$((${SZ_C} + ${SZ_PL} + ${SZ_JS} + ${SZ_HTML} + ${SZ_CSS} + ${SZ_SH} + ${SZ_JSON}))
+SZ_TTL=$((${SZ_C} + ${SZ_PL} + ${SZ_JS} + ${SZ_HTML} + ${SZ_CSS} + ${SZ_SH}))
 
-echo "LOC in $(basename $(pwd)): C ${SZ_C} Perl ${SZ_PL} JavaScript ${SZ_JS} HTML ${SZ_HTML} CSS ${SZ_CSS} BASH ${SZ_SH} JSON ${SZ_JSON} for a total of ${SZ_TTL} lines."
+echo "LOC in $(basename $(pwd)): C ${SZ_C} Perl ${SZ_PL} JavaScript ${SZ_JS} HTML ${SZ_HTML} CSS ${SZ_CSS} BASH ${SZ_SH} for a total of ${SZ_TTL} lines."
