d03981ec5bf49964b836e5430de3002f2d24c2a1
rustyaxe <netrig@istabpeople.com>
2025-05-19 21:00:34 -0400

    improved readability

--- install-deps.sh
+++ install-deps.sh
@@ -1,7 +1,9 @@
 [ -z "$PROFILE" ] && PROFILE=radio
 
-apt install -y libjson-perl libterm-readline-perl-perl libhash-merge-perl libjson-xs-perl libjson-perl libstring-crc32-perl libgpiod-dev libjpeg-dev libpipewire-0.3-dev libjpeg-dev libopus-dev
-apt install -y jq pkg-config libmbedtls-dev gpiod
+apt install -y libjson-perl libterm-readline-perl-perl libhash-merge-perl libjson-xs-perl \
+	       libjson-perl libstring-crc32-perl libgpiod-dev libjpeg-dev libpipewire-0.3-dev \
+	       libjpeg-dev libopus-dev jq pkg-config libmbedtls-dev gpiod
+
 cpan install Mojo::JSON::Pointer
 
 # Optional - for use with hamlib
