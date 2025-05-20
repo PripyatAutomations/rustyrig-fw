cba26672ca44ae04a2a04d9ea605dd777dd5e1f0
rustyaxe <netrig@istabpeople.com>
2025-05-19 21:04:39 -0400

    build changes to make more plesant ofr others

--- install-deps.sh
+++ install-deps.sh
@@ -1,14 +1,16 @@
 [ -z "$PROFILE" ] && PROFILE=radio
 
 apt install -y libjson-perl libterm-readline-perl-perl libhash-merge-perl libjson-xs-perl \
-	       libjson-perl libstring-crc32-perl libgpiod-dev libjpeg-dev libpipewire-0.3-dev \
-	       libjpeg-dev libopus-dev jq pkg-config libmbedtls-dev gpiod
+	       libjson-perl libstring-crc32-perl libgpiod-dev gpiod jq pkg-config libmdedtls-dev
+
 
 cpan install Mojo::JSON::Pointer
 
 # Optional - for use with hamlib
 USE_HAMLIB=$(cat config/${PROFILE}.config.json  | jq -r '.backend.hamlib')
 USE_SQLITE=$(cat config/${PROFILE}.config.json  | jq -r '.features.sqlite')
+USE_PIPEWIRE=$(cat config/${PROFILE}.config.json  | jq -r '.features.pipewire')
+USE_OPUS=$(cat config/${PROFILE}.config.json  | jq -r '.features.opus')
 
 if [ "$USE_HAMLIB" == "true" ]; then
    apt install -y libhamlib-dev libhamlib-utils
@@ -17,4 +19,12 @@ fi
 
 if [ "$USE_SQLITE" == "true" ]; then
    apt install -y sqlite3-tools libsqlite3-dev 
+fi
+
+if [ "$USE_PIPEWIRE" == "true" ]; then
+   apt install -y libpipewire-0.3-dev
+fi
+
+if [ "$USE_OPUS" == "true" ]; then
+   apt install -y libopus-dev
 fi
