88bf2eeb1d16b76e037553c40050b36f0cad4fad
Netrig Developers <netrig@istabpeople.com>
2025-06-06 07:46:25 -0400

    .

--- fwdsp-test.sh
+++ fwdsp-test.sh
@@ -2,23 +2,15 @@
 # Example of starting fwdsp for the audio side of things
 
 [ ! -z "$(pidof fwdsp.bin)" ] && killall -9 fwdsp.bin
-
 [ -z "$PROFILE" ] && PROFILE=radio
 
 # RX audio (mic in)
-#./build/${PROFILE}/fwdsp.bin -p "pulsesrc device=default ! audioconvert ! audioresample ! audio/x-raw,format=S16LE,rate=16000,channels=1 ! queue ! fdsink fd=%d" -c pcm 
-#./build/${PROFILE}/fwdsp.bin -p "pulsesrc device=default ! audioconvert ! audioresample ! audio/x-raw,format=S16LE,rate=44100,channels=1 ! queue ! fdsink fd=%d" -c pcm -s 44100
-#  -p "pulsesrc device=default ! audioconvert ! audioresample ! audio/x-raw,format=S16LE,rate=16000,channels=1 ! flacenc ! fdsink fd=%d" \
-
-#FORMAT="FLAC+OGG"
-
-#if [ "$FORMAT" == "FLAC+OGG" ]; then
 #   rx_pipeline="pulsesrc device=default ! audioconvert ! audioresample ! opusenc ! oggmux ! queue ! fdsink fd=%d"
 #   rx_pipeline="pulsesrc device=default ! audioconvert ! audioresample ! flacenc ! oggmux ! queue ! fdsink fd=%d"
    rx_pipeline="pulsesrc device=default ! audioconvert ! audioresample ! audio/x-raw,format=S16LE,rate=16000,channels=1 ! queue ! fdsink fd=%d"
+#   rx_pipeline="pulsesrc device=default ! audioconvert ! audioresample ! audio/x-raw,format=S16LE,rate=44100,channels=1 ! flacenc ! queue ! fdsink fd=%d"
 #   tx_pipepine="fdsrc fd=%d ! oggdemux ! flacdec ! audioconvert ! audioresample ! queue ! pulsesink device=default"
-#fi
+
 export GST_DEBUG=*:3
-#./build/${PROFILE}/fwdsp.bin -p "${rx_pipeline}" -c flac -s 44100 &
-./build/${PROFILE}/fwdsp.bin -p "${rx_pipeline}" -s 44100 &
-#./build/${PROFILE}/fwdsp.bin -p "${tx_pipeline}" -c flac -s 44100 -t &
+./build/${PROFILE}/fwdsp.bin -p "${rx_pipeline}" &
+#./build/${PROFILE}/fwdsp.bin -p "${tx_pipeline}" -t &
--- gtk-client/audio.c
+++ gtk-client/audio.c
@@ -49,9 +49,9 @@ bool ws_audio_init(void) {
       NULL);
 #else
    rx_pipeline = gst_parse_launch("appsrc name=mysrc is-live=true format=bytes "
-   " ! oggdemux "
-//   " ! flacdec "
-   " ! opusdec "
+//   " ! oggdemux "
+   " ! flacdec "
+//   " ! opusdec "
    " ! audioconvert "
    " ! audioresample "
    " ! queue "
@@ -74,11 +74,11 @@ rx_pipeline = gst_parse_launch(
    if (!appsrc) return false;
 
    g_object_set(G_OBJECT(appsrc),
-#if	defined(AUDIO_PCM)
-                "format", GST_FORMAT_TIME,
-#else
+//#if	defined(AUDIO_PCM)
+//                "format", GST_FORMAT_TIME,
+//#else
                 "format", GST_FORMAT_BYTES,
-#endif
+//#endif
                 "is-live", TRUE,
                 "stream-type", 0, // GST_APP_STREAM_TYPE_STREAM
                 NULL);
