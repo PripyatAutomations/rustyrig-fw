41f637262aa9b0c450a53fc5bb2f16b61347588f
Pripyat Automations <84538339+PripyatAutomations@users.noreply.github.com>
2025-06-19 14:02:34 -0400

    win32

--- src/rrclient/mk/win32.mk
+++ src/rrclient/mk/win32.mk
@@ -1,3 +1,3 @@
 win32-deps:
 	pacman -Syu
-	pacman -S mingw-w64-x86_64-toolchain mingw-w64-x86_64-gtk3 mingw-w64-x86_64-gstreamer
+	pacman -S mingw-w64-x86_64-toolchain mingw-w64-x86_64-gtk3 mingw-w64-x86_64-gstreamer mingw-w64-x86_64-gst-plugins-base
