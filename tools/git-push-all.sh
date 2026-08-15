04ded1e66cd64f0fd0f3b096a1e411cfa4c2d4d5
Pripyat Automations <dev@istabpeople.com>
2026-08-15 19:51:16 -0400

    fixed makefiles

--- tools/git-push-all.sh
+++ tools/git-push-all.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 P=$(pwd)
 
-if [ ! -z "$1" ]; then
-   MSG="-m \"$1\""
+cd librustyaxe
+if [ -z "$1" ]; then
+   git commit -a
+else
+   git commit -a -m "${1}"
 fi
 
-cd librustyaxe
-git commit -a ${MSG}
 git push
 cd "${P}"
 
 cd librrprotocol
-git commit -a ${MSG}
+if [ -z "$1" ]; then
+   git commit -a
+else
+   git commit -a -m "${1}"
+fi
+
 git push
 cd "${P}"
 
 cd www
-git commit -a ${MSG}
+if [ -z "$1" ]; then
+   git commit -a
+else
+   git commit -a -m "${1}"
+fi
 git push
 cd "${P}"
 
-git commit -a ${MSG}
+if [ -z "$1" ]; then
+   git commit -a
+else
+   git commit -a -m "${1}"
+fi
 git push
