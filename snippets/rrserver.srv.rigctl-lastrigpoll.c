f0b3fc57e3683d0f4a20d1e2a3caab4cd51564ac
Pripyat Automations <dev@istabpeople.com>
2026-08-21 10:23:49 -0400

    .

---
+++ snippets/rrserver.srv.rigctl-lastrigpoll.c
@@ -0,0 +1,5 @@
+         // XXX: We should do a latency test at the start of the session and
+         // optimize this per-user from there
+// XXX: readd this
+//         last_rig_poll.tv_sec = (loop_start.tv_sec + HTTP_API_RIGPOLL_PAUSE);
+//         last_rig_poll.tv_nsec = loop_start.tv_nsec;
